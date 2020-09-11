e827aec7c48f0959038370dffcaa3a5b36c0dc17
TBudzynski <toomoo199@wp.pl>
2020-09-11 21:05:54 +0200

    dodanie komentarza do klasy KcalCalculator

--- src/main/java/com/company/KcalCalculator.java
+++ src/main/java/com/company/KcalCalculator.java
@@ -14,10 +14,12 @@ public class KcalCalculator extends Calory implements Serializable {
         public double obliczKcal() {
             if (getSex() == 1) {
                 kcal = ((9.9 * getBodyweight() + 625 * getHeight()) - 4.29 * getAge() + 5) * getPhysicalactivity()+DecisionClass.getRoznica();
+                System.out.println("Obliczylem ci kcal dziku");
             }
 
             if (getSex() == 2) {
                 kcal = ((9.99 * getBodyweight() + 625 * getHeight()) - 4.29 * getAge() - 161) * getPhysicalactivity()+DecisionClass.getRoznica();
+                System.out.println("Obliczylem ci kcal dziczko");
             }
             return kcal;
         }
